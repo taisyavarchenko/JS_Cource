d44e5daa27d7706dce918d7f200b176b057ce553
Taisya Varchenko <taisya.varchenko@mail.ru>
2021-11-12 01:05:24 +0300

    Tabs

---
+++ Tabs/index.js
@@ -0,0 +1,12 @@
+$(function() {
+	var tab = $('#tabs .tabs-items > div'); 
+	tab.hide().filter(':first').show(); 
+	
+	$('#tabs .tabs-nav a').click(function(){
+		tab.hide(); 
+		tab.filter(this.hash).show(); 
+		$('#tabs .tabs-nav a').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	}).filter(':first').click();	
+});

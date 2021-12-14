1649fdd4429b7bac6dbca0e031087733125d5775
taisyavarchenko <55127370+taisyavarchenko@users.noreply.github.com>
2021-12-14 23:36:50 +0300

    Fix

--- VueJS/3.js
+++ VueJS/3.js
@@ -3,9 +3,9 @@ var app = new Vue({
     data: {
       message: '',
       articals: [
-        { title: 'JavaScript',url: 'https://ru.wikipedia.org/wiki/JavaScript' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/80px-Unofficial_JavaScript_logo_2.svg.png', seen: false  },
-        { title: 'Vue.js',url: 'https://ru.wikipedia.org/wiki/Vue.js' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/93px-Vue.js_Logo_2.svg.png', seen: false },
-        { title: 'React.js',url: 'https://ru.wikipedia.org/wiki/React' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/113px-React-icon.svg.png', seen: false }
+        { title: 'JavaScript',url: 'https://ru.wikipedia.org/wiki/JavaScript' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/80px-Unofficial_JavaScript_logo_2.svg.png', seen: true  },
+        { title: 'Vue.js',url: 'https://ru.wikipedia.org/wiki/Vue.js' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/93px-Vue.js_Logo_2.svg.png', seen: true },
+        { title: 'React.js',url: 'https://ru.wikipedia.org/wiki/React' ,image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/113px-React-icon.svg.png', seen: true }
       ],
     },
     methods: {
